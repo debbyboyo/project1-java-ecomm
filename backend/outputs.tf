e4e732bba5039417a07ba309398b0c440451ee18
debbyboyo <boyodebby@gmail.com>
2025-06-01 00:26:46 +0100

    Update outputs.tf

--- backend/outputs.tf
+++ backend/outputs.tf
@@ -1,9 +1,17 @@
-output "workstation_public_ip" {
-  value = aws_instance.workstation.public_ip
+output "master_public_ip" {
+  description = "Public IP of the Kubernetes Master node"
+  value       = aws_instance.k8s_master.public_ip
 }
-output "master_private_ip" {
-  value = aws_instance.master.private_ip
-}
+
 output "worker_private_ips" {
-  value = [for i in aws_instance.worker : i.private_ip]
+  description = "Private IPs of the Kubernetes Worker nodes"
+  value       = [
+    aws_instance.k8s_worker_1.private_ip,
+    aws_instance.k8s_worker_2.private_ip
+  ]
+}
+
+output "master_private_ip" {
+  description = "Private IP of the Kubernetes Master node"
+  value       = aws_instance.k8s_master.private_ip
 }

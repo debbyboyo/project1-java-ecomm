41ea90b8b4459157798f40022e4c78edb6e2c8c9
debbyboyo <boyodebby@gmail.com>
2025-05-31 16:08:32 +0100

    update on mainjava

--- backend/AppController.java
+++ backend/AppController.java
@@ -8,6 +8,6 @@ public class AppController {
     @GetMapping("/")
     public String home() {
         return "<html><head><style>body { font-family: Arial; background: #f4f4f9; color: #333; text-align: center; padding: 50px; }</style></head>" +
-               "<body><h1>Welcome to CEEYIT E-Commerce Backend</h1><p>This is a sample API running on Spring Boot.</p></body></html>";
+               "<body><h1>Welcome to DEBORAH BOYO project FINALE Backend</h1><p>This is a sample API running on Spring Boot.</p></body></html>";
     }
 }

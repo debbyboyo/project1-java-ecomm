3fc683afe9b44339e27092d4f84c73907203ac35
debbyboyo <boyodebby@gmail.com>
2025-06-01 00:03:20 +0100

    Update variables.tf

--- backend/variables.tf
+++ backend/variables.tf
@@ -1,4 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
 variable "key_name" {
-  description = "project3"
-  type        = string
+  default = "butter"
+}
+
+variable "instance_ami" {
+  default = "ami-0c2b8ca1dad447f8a" # Amazon Linux 2023 AMI
+}
+
+variable "instance_type" {
+  default = "t2.medium"
 }

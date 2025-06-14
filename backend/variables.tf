2824a6ef7ec695e764e8b07db64b9966918d8947
debbyboyo <boyodebby@gmail.com>
2025-06-14 07:33:31 +0100

    eks variables.tf

--- backend/variables.tf
+++ backend/variables.tf
@@ -1,15 +1,8 @@
-variable "aws_region" {
-  default = "us-east-1"
+variable "region" {
+  default = "us-east-2"
 }
 
 variable "key_name" {
-  default = "butter"
-}
-
-variable "instance_ami" {
-  default = "ami-0c2b8ca1dad447f8a" # Amazon Linux 2023 AMI
-}
-
-variable "instance_type" {
-  default = "t2.medium"
+  description = "EC2 key pair for SSH access"
+  type        = string
 }
